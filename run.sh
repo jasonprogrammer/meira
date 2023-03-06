fda7a6a0bedff9b0126195be56ec6f3a4c08d847
Jason Jones <jason.jones.programmer@gmail.com>
2023-03-05 19:05:26 -0700

    Add docs

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 rm -r ~/.nimble/pkgs2/meira*
 nimble install
-nim c examples/static_files.nim && ./examples/static_files
+# nim c examples/static_files.nim && ./examples/static_files
+nim c examples/url_params.nim && ./examples/url_params
 
 
