baddc7098d4952e07b1ff6c3d1f2167a312c1514
Jason Jones <jason.jones.programmer@gmail.com>
2023-03-11 17:29:36 -0700

    Add example to parse and return JSON

--- run_example.sh
+++ run_example.sh
@@ -2,6 +2,7 @@ rm -r ~/.nimble/pkgs2/meira*
 nimble install
 # nim c examples/static_files.nim && ./examples/static_files
 # nim c examples/url_params.nim && ./examples/url_params
-nim c examples/submit_forms.nim && ./examples/submit_forms
+# nim c examples/submit_forms.nim && ./examples/submit_forms
+nim c examples/submit_json.nim && ./examples/submit_json
 
 
